5c3ac2f8a330822c4113b57a3113a6d392125e5d
Erhan ÜRGÜN <erhan.rgn04@gmail.com>
2021-12-12 19:24:33 +0300

    ufak tefek değişiklikler - 2

--- mail_gonderme/frmMailGonder.Designer.cs
+++ mail_gonderme/frmMailGonder.Designer.cs
@@ -30,6 +30,7 @@ namespace mail_gonderme
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMailGonder));
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.cmbGonderici = new System.Windows.Forms.ComboBox();
@@ -241,6 +242,7 @@ namespace mail_gonderme
             this.Controls.Add(this.label5);
             this.Controls.Add(this.tabControl1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmMailGonder";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
